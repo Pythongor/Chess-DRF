05196340ed71d7ee11cefd1a85bdb6a69cec9f83
Demitrius <djmozartd@gmail.com>
2019-09-16 17:23:59 +0300

    fix incorrect rearchitect

--- Chess/Server/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('get_token', views.login),
-    path('users', views.UserView.as_view()),
-    path('users/<pk>', views.UserView.as_view()),
-]
